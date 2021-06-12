c69df068f74741a4d42441db9fdc7bb40a2e3ee9
Joao-amoedo <jp.advc@gmail.com>
2021-06-11 22:43:35 -0300

    Perfil do professor

---
+++ app/src/main/java/com/example/gymout/activitys/ActivityPerfilProfessor.kt
@@ -0,0 +1,12 @@
+package com.example.gymout.activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gymout.R
+
+class ActivityPerfilProfessor : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil_professor)
+    }
+}
