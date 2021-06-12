aca60fc44d77dc04d4525d62c474b5433350ae6e
Joao-amoedo <jp.advc@gmail.com>
2021-06-11 21:09:08 -0300

    Criando recycleview

---
+++ app/src/main/java/com/example/gymout/activitys/ActivityTelaPrincipalAluno.kt
@@ -0,0 +1,12 @@
+package com.example.gymout.activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gymout.R
+
+class ActivityTelaPrincipalAluno : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_principal_aluno)
+    }
+}
